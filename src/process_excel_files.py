d6fed7826bda5c909767d3ab335cd21c9be23ab1
John Louie <jlouie95618@gmail.com>
2016-05-16 20:35:23 -0700

    Added source code files for clustering and parsing Excel files

--- src/process_excel_files.py
+++ src/process_excel_files.py
@@ -1,9 +1,15 @@
 import sys
-import pandas
+import pandas as pd
+import numpy as np
+
+
+
 
 def main(args):
     print args
-    
+    if len(args) < 1: return
+    file = pd.read_excel(args[0], "Sheet1")
+    print file
 
 if __name__ == '__main__':
     main(sys.argv[1:])
