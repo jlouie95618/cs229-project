bfc893f81c9f319c22c03987f7776bf2a8469f74
John Louie <jlouie95618@gmail.com>
2016-05-17 01:37:10 -0700

    got clustering code to work with some jank csv files

--- src/cluster_medicare_data.py
+++ src/cluster_medicare_data.py
@@ -3,10 +3,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
+from sklearn import datasets
+
+# def read_file():
+#     with open(filename) as f:
+#         for line in f:
+
+
 def main(args):
     print args
     if len(args) < 1: return
-    X = np.loadtxt(args[0])
+    X = np.genfromtxt(args[0], delimiter=',')
+    print X 
+    for x in X:
+        if np.isnan(np.sum(x)): print x
+    # y = np.loadtxt(args[1])
+    estimators = {'k_means_3': KMeans(n_clusters=3),
+                    'k_means_4': KMeans(n_clusters=4),
+                    'k_means_5': KMeans(n_clusters=5),
+                    'k_means_6': KMeans(n_clusters=6),
+                    'k_means_7': KMeans(n_clusters=7),
+                    'k_means_8': KMeans(n_clusters=8) }
+
+    for name, est in estimators.items():
+        est.fit(X)
+        values = est.cluster_centers_
+        labels = est.labels_
+        print name
+        print values
+        print labels
+        for i in xrange(0, max(labels) + 1):
+            count = 0
+            for val in labels:
+                if val == i: count += 1
+            print str(i) + " : " + str(count) 
+
 
 
 if __name__ == '__main__':
