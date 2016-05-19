bae8e20f3779b947476e75c5e920869fac004e4e
awells2 <awells2@stanford.edu>
2016-05-18 21:41:18 -0700

    added svm.py and changed linear_regression.py

--- src/svm.py
+++ src/svm.py
@@ -1,34 +1,33 @@
 import sys
-from sklearn import linear_model
-from sklearn import datasets
-from sklearn import preprocessing
-from sklearn.metrics import explained_variance_score
-from sklearn.metrics import r2_score
 import numpy as np
-
+from sklearn.svm import SVR
+from sklearn.metrics import explained_variance_score
+from sklearn import preprocessing
 
 def main(args):
-	if len(sys.argv) < 2:
-		print("USAGE: python linear_regression.py [hrr_data_total3.csv] [hrr_data_labels.csv]")
+	if len(args) < 2:
+		print("USAGE python svm.py [X_data] [Y_data]")
 		exit(0)
 	X = np.genfromtxt(args[0], delimiter=',')
-	X = process_X(X)
 	X = preprocessing.scale(X)
-	Y = parse_Y(sys.argv[2])
+	X = process_X(X)
+	Y = parse_Y(args[1])
 	cutoff = int(len(X)*.7)
 	X_train = X[:cutoff]
 	Y_train = Y[:cutoff]
 	X_test = X[(cutoff+1):]
 	Y_test = Y[(cutoff+1):]
-	regr = linear_model.LinearRegression()
-	regr.fit(X_train,Y_train)
+	clf = SVR(kernel='rbf',C=1e4,degree=3)
+	clf.fit(X_train,Y_train)
+	pred = clf.predict(X_test)
 	print("Residual sum of squares: %.2f"
-      % np.mean((regr.predict(X_test) - Y_test) ** 2))
-	pred = regr.predict(X_test)
+      % np.mean((clf.predict(X_test) - Y_test) ** 2))
+	pred = clf.predict(X_test)
 	# Explained variance score: 1 is perfect prediction
-	print('Variance score: %.8f' % regr.score(X_test, Y_test))
-
+	print("Variance Score: %.4f" % explained_variance_score(Y_test,pred,multioutput='uniform_average'))
 	return
+
+
 
 def parse_Y(Y_f):
 	Y_arr = np.empty((0))
@@ -43,11 +42,6 @@ def process_X(X):
 	X_new = np.delete(X,0,1)
 	X_new = np.delete(X_new,0,1)
 	return X_new
-
-
-
-
-
 
 
 if __name__ == "__main__":
